22d26bb399e893841fa75957a4a4f579ce9e89f7
samkoi <samitkoirala@hotmail.com>
2021-12-11 17:37:24 +0100

    Added controller and templates

--- src/main/java/com/samit/springBootTaco/SpringBootTacoApplication.java
+++ src/main/java/com/samit/springBootTaco/SpringBootTacoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootTacoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootTacoApplication.class, args);
 	}
 
