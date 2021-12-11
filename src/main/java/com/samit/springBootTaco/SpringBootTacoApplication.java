3f7cc37ba23b40e83e52c135bdae35b4c94f2238
Samit Koirala <samit.koirala@stud.uni-bamberg.de>
2021-12-11 16:20:30 +0100

    initial commit

---
+++ src/main/java/com/samit/springBootTaco/SpringBootTacoApplication.java
@@ -0,0 +1,13 @@
+package com.samit.springBootTaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samit/springBootTaco/SpringBootTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samit.springBootTaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
